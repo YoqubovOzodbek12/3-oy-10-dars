67772f3293499745c84267e74b813bde298f1369
YoqubovOzodbek12 <ozodbekyoqubob575@gmail.com>
2022-11-28 21:16:39 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,20 @@
+// 1-misol
+
+// function count(para1, para2) {
+//     let result = para1.push(para2)
+
+//     return result
+// }
+
+// console.log(count(['salom','alik','nok',], 'salom'));
+
+
+
+// 2-misol
+
+// function func(para1) {
+
+//     return para1[para1.length-1]
+// }
+
+// console.log(func(['salom','nok','olma',]))
