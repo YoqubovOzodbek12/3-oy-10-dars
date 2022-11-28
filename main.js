3610b67731c27fd4bc7a53c148025654c731513b
YoqubovOzodbek12 <ozodbekyoqubob575@gmail.com>
2022-11-28 21:50:52 +0300

    add 1-misol

--- main.js
+++ main.js
@@ -1,12 +1,16 @@
 // 1-misol
 
-// function count(para1, para2) {
-//     let result = para1.push(para2)
+function count(para1, para2) {
+    let result = 0;
+    for(let i = 0; i < para1.length; i++)
+    if(para1[i] == para2) {
+        result = i
+    }
 
-//     return result
-// }
+    return result
+}
 
-// console.log(count(['salom','alik','nok',], 'salom'));
+console.log(count(['salom','alik','nok',], 'nok'));
 
 
 
